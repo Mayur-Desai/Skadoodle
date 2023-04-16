c55ebefbc91ff70043676de3f1af53a8f3d9dd9f
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2023-04-16 21:45:45 +0530

    Add files via upload

---
+++ candidates_list.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:wid_learn/app_screens/player_list.dart';
+import '../models/gamer.dart';
+import './ingame_participants.dart';
+class candidateList extends StatefulWidget {
+  bool iswaiting;
+  bool isAdmin;
+  final CollectionReference roomParticipants;
+  candidateList({required this.iswaiting,required this.isAdmin,required this.roomParticipants});
+  @override
+  State<candidateList> createState() => _candidateListState(iswaiting: iswaiting,isAdmin: isAdmin,roomParticipants: roomParticipants);
+}
+
+class _candidateListState extends State<candidateList> {
+  bool iswaiting;
+  bool isAdmin;
+  final CollectionReference roomParticipants;
+  _candidateListState({required this.iswaiting,required this.isAdmin,required this.roomParticipants});
+  @override
+  Widget build(BuildContext context) {
+    final candidates = Provider.of<List<Gamer>?>(context) ?? [];
+    return getPlayerlist(candidates);
+  }
+  Widget getPlayerlist(candidates){
+    var listview = ListView.builder(
+      itemCount: candidates.length,
+      itemBuilder: (context,index){
+        // if(iswaiting){
+        //   return playerTile(gamer: candidates[index],isAdmin: isAdmin,roomParticipants: roomParticipants);
+        // }
+        // else{
+        //   return InGameplayerTile(gamer: candidates[index],isAdmin: isAdmin,roomParticipants: roomParticipants);
+        // }
+        return InGameplayerTile(gamer: candidates[index],iswaiting: iswaiting,roomParticipants: roomParticipants,isAdmin: isAdmin);
+      },
+    );
+    return listview;
+  }
+}
+
+
